62412f15bef915b2e8212f3a20922ef4474be8dd
amirhasan202 <amirhasan202@gmail.com>
2023-11-14 14:39:14 +0330

    use of PUT and POST

--- src/compo/todo/TodoApiServices.js
+++ src/compo/todo/TodoApiServices.js
@@ -15,3 +15,10 @@ export const retrieveAllTodosForUsernameApi
 
 export const deleteTodoApi
         = (username , id) => apiClient.delete(`/users/${username}/todos/${id}`)
+
+
+export const retrieveTodoApi
+        = (username , id) => apiClient.get(`/users/${username}/todos/${id}`)
+
+export const updateTodoApi
+        = (username , id , todo ) => apiClient.put(`/users/${username}/todos/${id}` , todo)
--- src/compo/todo/TodoComponent.jsx
+++ src/compo/todo/TodoComponent.jsx
@@ -1,9 +1,87 @@
+import {  useNavigate, useParams } from "react-router-dom"
+import { retrieveTodoApi, updateTodoApi } from "./TodoApiServices"
+import { useEffect, useState } from "react"
+import { Field,Form, Formik } from "formik"
+
 export default function Todocomponent ()
 {
-    return 
-    (
-        <div className="Container"> 
+
+    const {id} = useParams()
+
+    // const authContext = useAuth()
+    
+    // const username = authContext.username
+
+    const [description , setDescription]=useState('')
+
+    const [targetdate , setTargetDate]=useState('')
+
+    useEffect(
+        () => retrieveTodos() 
+        , [id]
+    )
+
+    const navigate = useNavigate()
+
+    function retrieveTodos(){
+        retrieveTodoApi('in28minutes' , id)
+            .then(response => {
+                setDescription(response.data.description)
+                setTargetDate(response.data.targetdate)
+            })
+            .catch(error => console.log(error))
+    }
+
+    function onSubmit (values){
+        console.log(values)
+        const todo = {
+            id : id,
+            username : 'in28minutes',
+            description : values.description,
+            targetdate : values.targetdate,
+            done : false
+        }
+        console.log(todo)
+        updateTodoApi('in28minutes', id ,todo)
+            .then(response => {
+                navigate('/todos')
+
+            })
+            .catch(error => console.log(error))
+    }
+
+
+
+
+    return (
+        <div className="container"> 
             <h1> Enter Your Todo Details HERE </h1>
+            <div>
+                <Formik initialValues={ {description , targetdate} }
+                     enableReinitialize={true}
+                     onSubmit={onSubmit}
+                     >
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field type="text" className="form-control" name="description"/>
+                                </fieldset> 
+
+                               <fieldset className="form-group">
+                                    <label>TargetDate</label>
+                                    <Field type="date" className="form-control" name="targetdate" />
+                                </fieldset> 
+                                <div>
+                                    <button className="btn btn-success m-5" type="submit">Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
+            </div>
         </div>
+
     )
 }
